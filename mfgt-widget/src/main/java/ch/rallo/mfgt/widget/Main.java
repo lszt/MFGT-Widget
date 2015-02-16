8d49d43c978a28aded985216c4ce14472ac91b80
Roland Züger <rzueger@tocco.ch>
2015-02-16 06:00:43 +0100

    Ensure that always the called activity is visible

--- mfgt-widget/src/main/java/ch/rallo/mfgt/widget/Main.java
+++ mfgt-widget/src/main/java/ch/rallo/mfgt/widget/Main.java
@@ -31,6 +31,7 @@ public class Main extends AppWidgetProvider {
 
 	private PendingIntent getActivity(Context context, Class cls) {
 		Intent intent = new Intent(context, cls);
+		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		return PendingIntent.getActivity(context, 0, intent, 0);
 	}
 
